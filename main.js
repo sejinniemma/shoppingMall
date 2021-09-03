20bb731ba3e06d545dd087bed6411b393817fabd
Emma <jsj0471@naver.com>
2021-09-03 17:19:11 +0900

    js!

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 'use strict';
 
-
-
 // Fetch
 function loadItems(){
     return fetch('data/data.json')
@@ -14,20 +12,37 @@ function displayItems(items){
   }
 
   function creatHTMLString(item){
-  return  `
-    <li class="item">
+  return  `<li class="item">
              <img class="item__thumbnail" src=${item.image} alt="" >
              <span class="item__description">${item.gender}, ${item.size}</span>
-         </li>
-        `;
+         </li>`;
   }
 
   // main
 loadItems()
  .then(items => {
     displayItems(items);
-    // setEventListener(items);
+    setEventListener(items);
  })
- .catch(console.log('error'))
+ .catch(console.log)
 
 
+
+function onItemClick(event,items){
+  const dataset = event.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
+
+  if(key == null || value == null){
+    return;
+  }
+
+  displayItems(items.filter(item => item[key] === value))
+}
+
+function setEventListener(items){
+  const logo = document.querySelector('.logo');
+  logo.addEventListener('click',() => displayItems(items));
+  const buttons = document.querySelector('.buttons');
+  buttons.addEventListener('click', event => onItemClick(event,items));
+}
