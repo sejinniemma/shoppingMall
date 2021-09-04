d96de587125a1c7af8fd640b90b7033725e97008
Emma <jsj0471@naver.com>
2021-09-04 23:41:54 +0900

    try to use visible

--- main.js
+++ main.js
@@ -1,48 +1,65 @@
 'use strict';
+ 
 
-// Fetch
-function loadItems(){
+ // main
+  loadItems()
+  .then(items => {
+     displayItems(items);
+     setEventListener(items);
+  })
+  .catch(console.log)
+
+
+  // Fetch
+function loadItems() {
     return fetch('data/data.json')
     .then(response => response.json())
     .then(json => json.items);
 }
-function displayItems(items){
-    const container = document.querySelector('.items');
+
+
+function displayItems(items) {
+  const container = document.querySelector('.items');
     container.innerHTML = items.map(item => creatHTMLString(item))
   }
 
-  function creatHTMLString(item){
+  function creatHTMLString(item) {
   return  `<li class="item">
              <img class="item__thumbnail" src=${item.image} alt="" >
              <span class="item__description">${item.gender}, ${item.size}</span>
          </li>`;
   }
 
-  // main
-loadItems()
- .then(items => {
-    displayItems(items);
-    setEventListener(items);
- })
- .catch(console.log)
 
 
+  function setEventListener(items){
+    const logo = document.querySelector('.logo');
+    logo.addEventListener('click',() => displayItems(items));
+    const buttons = document.querySelector('.buttons');
+    buttons.addEventListener('click', event => onItemClick(event,items));
+  }
 
-function onItemClick(event,items){
-  const dataset = event.target.dataset;
-  const key = dataset.key;
-  const value = dataset.value;
+
+ 
+  function onItemClick(event,items){
+  const target = event.target;
+  const key = target.dataset.key;
+  const value = target.dataset.value;
 
   if(key == null || value == null){
     return;
   }
 
-  displayItems(items.filter(item => item[key] === value))
+  // displayItems(items.filter(item => item[key] === value))
+  updateItems(items,key,value);
 }
 
-function setEventListener(items){
-  const logo = document.querySelector('.logo');
-  logo.addEventListener('click',() => displayItems(items));
-  const buttons = document.querySelector('.buttons');
-  buttons.addEventListener('click', event => onItemClick(event,items));
-}
+ function updateItems(items,key,value){
+   items.forEach(item => {
+     if(item[key] === value){
+      item.classList.remove('invisible');
+     }else{
+      item.classList.add('invisible');
+     }
+   });
+ }
